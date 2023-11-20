a353d75544567db4376aa31d7653d06ac3e980aa
Sajid2001 <60523377+Sajid2001@users.noreply.github.com>
2023-11-19 23:37:27 -0500

    npm package changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
+const rename = require('gulp-rename');
 
 function buildStyles(){
     return src('sass/**/*.scss')
         .pipe(sass())
         .pipe(purgecss({content: ['*.html']}))
-        .pipe(dest('css'))
+        .pipe(rename('yoicards.css'))
+        .pipe(dest('yoi-dist'))
 }
 
 function watchTask(){
